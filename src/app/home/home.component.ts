4e1b85e8fec276b48ef6348a2e8abe5dda09a935
sakiladissanayake <sakilad@gmail.com>
2020-03-22 23:47:36 +0530

    add sample data set

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,7 +7,7 @@ import { DataService} from '../data.service';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  data: any ;
+  data: any = {"success":true,"message":"Success","data":{"update_date_time":"2020-03-22 17:50  :50","local_new_cases":0,"local_total_cases":0,"local_total_number_of_individuals_in_hospitals":0,"local_deaths":0,"local_new_deaths":0,"local_recovered":0,"global_new_cases":0,"global_total_cases":0,"global_deaths":0,"global_new_deaths":0,"global_recovered":0}};
 
   constructor(private dataService: DataService) { }
 
